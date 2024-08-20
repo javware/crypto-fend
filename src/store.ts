bc1ab1e0c7770a9c67024cc116441b7d00df49d9
javware <xaviercito052018@gmail.com>
2024-08-20 18:58:42 -0500

    add new proyect

---
+++ src/store.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { Cryptocurrency, CryptoPrice, Pair } from './types'
+import { getCryptos, fetchCurrentCryptoPrice } from './services/CryptoService'
+
+type CryptoStore = {
+    cryptocurrencies: Cryptocurrency[]
+    result: CryptoPrice
+    loading: boolean
+    fetchCryptos: () => Promise<void>
+    fetchData: (pair: Pair) => Promise<void>
+}
+
+export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
+    cryptocurrencies: [],
+    result: {} as CryptoPrice,
+    loading: false,
+    fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+            cryptocurrencies
+        }))
+    },
+    fetchData: async (pair) => {
+        set(() => ({
+            loading: true
+        }))
+        const result = await fetchCurrentCryptoPrice(pair)
+        set(() => ({
+            result,
+            loading: false
+        }))
+    }
+
+})))
